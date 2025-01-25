f13a321b05b02232487514ba6561ef8152c26924
Patric Gekoski <=>
2025-01-25 16:49:23 -0500

    Convert models to typescript

--- models/category.ts
+++ models/category.ts
@@ -1,9 +1,5 @@
 class Category {
-  constructor(id, title, color) {
-    this.id = id;
-    this.title = title;
-    this.color = color;
-  }
+	constructor(public id: string, public title: string, public color: string) {}
 }
 
-export default Category;
+export default Category
--- models/meal.ts
+++ models/meal.ts
@@ -1,32 +1,48 @@
+interface MealInput {
+	id: string
+	categoryIds: Array<string>
+	title: string
+	affordability: string
+	complexity: string
+	imageUrl: string
+	duration: number
+	ingredients: Array<string>
+	steps: Array<string>
+	isGlutenFree: boolean
+	isVegan: boolean
+	isVegetarian: boolean
+	isLactoseFree: boolean
+}
+
 class Meal {
-	constructor(
-		id,
-		categoryIds,
-		title,
-		affordability,
-		complexity,
-		imageUrl,
-		duration,
-		ingredients,
-		steps,
-		isGlutenFree,
-		isVegan,
-		isVegetarian,
-		isLactoseFree,
-	) {
-		this.id = id
-		this.categoryIds = categoryIds
-		this.title = title
-		this.imageUrl = imageUrl
-		this.ingredients = ingredients
-		this.steps = steps
-		this.duration = duration
-		this.complexity = complexity
-		this.affordability = affordability
-		this.isGlutenFree = isGlutenFree
-		this.isVegan = isVegan
-		this.isVegetarian = isVegetarian
-		this.isLactoseFree = isLactoseFree
+	public id: string
+	public categoryIds: Array<string>
+	public title: string
+	public affordability: string
+	public complexity: string
+	public imageUrl: string
+	public duration: number
+	public ingredients: Array<string>
+	public steps: Array<string>
+	public isGlutenFree: boolean
+	public isVegan: boolean
+	public isVegetarian: boolean
+	public isLactoseFree: boolean
+
+	constructor(data: MealInput) {
+		this.id = data.id
+		this.categoryIds = data.categoryIds
+		this.title = data.title
+		this.imageUrl = data.imageUrl
+		this.ingredients = data.ingredients
+		this.steps = data.steps
+		this.duration = data.duration
+		this.complexity = data.complexity
+		this.affordability = data.affordability
+		this.isGlutenFree = data.isGlutenFree
+		this.isVegan = data.isVegan
+		this.isVegetarian = data.isVegetarian
+		this.isLactoseFree = data.isLactoseFree
 	}
 }
 
